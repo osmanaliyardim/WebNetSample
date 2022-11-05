c68e249b464ac41271fb4c6bf61040d282ed10ff
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-11-05 22:36:29 +0300

    REFACTOR: Alignments
    Some code alignments and namings.

--- WebNetSample/Controllers/HomeController.cs
+++ WebNetSample/Controllers/HomeController.cs
@@ -5,18 +5,9 @@ namespace WebNetSample.WebNetMVC.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ICategoryService _categoryService;
-
-    public HomeController(ICategoryService categoryService)
-    {
-        _categoryService = categoryService; 
-    }
-
     [HttpGet]
     public IActionResult Index()
     {
-        var categoryImages = _categoryService.GetAllAsync();
-
-        return View(categoryImages.Result);
+        return RedirectToAction("Index", "Categories");
     }
 }
