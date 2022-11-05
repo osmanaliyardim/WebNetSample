17d2c7988e46cb867ba50a852cb722dc436cf783
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-11-06 00:59:53 +0300

    FEATURE: ViewComponenets
    ViewComponents have been created for both Products and Categories.

---
+++ WebNetSample/Components/ProductsWidget.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebNetSample.Business.Abstract;
+
+namespace WebNetSample.WebNetMVC.Components;
+
+public class ProductsWidget : ViewComponent
+{
+    private readonly IProductService _productService;
+
+    public ProductsWidget(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var products = await _productService.GetProductDetailsAsync();
+
+        return View(products);
+    }
+}
