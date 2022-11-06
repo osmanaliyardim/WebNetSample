8d0c23033b436bb333a719f44121b4ccf0bec73f
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-11-06 20:47:52 +0300

    FEATURE: Custom Tag
    Custom tag helper has been created for category images.

---
+++ Core/Extensions/ImageLinkTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebNetSample.Core.Extensions;
+
+[HtmlTargetElement("imagelink", Attributes = "webnetsample-id")]
+public class ImageLinkTagHelper : TagHelper
+{
+    [HtmlAttributeName("webnetsample-id")]
+    public string id { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "div";
+        output.Content.AppendHtml($@"<a href=""/Categories/GetImageById/id={id}"" alt=""Click to see image in a new tab""></a>");
+        output.Attributes.Clear();
+    }
+}
