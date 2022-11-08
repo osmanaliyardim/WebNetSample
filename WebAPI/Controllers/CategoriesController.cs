cdc2b2a822ebc7a695a77b3cd0de2db743b79538
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-11-08 19:32:17 +0300

    FEATURE: Endpoints
    New endpoints have been created for both categories and products.

---
+++ WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebNetSample.Business.Abstract;
+
+namespace WebNetSample.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoriesController : ControllerBase
+{
+    private readonly ICategoryService _categoryService;
+
+    public CategoriesController(ICategoryService categoryService)
+    {
+        _categoryService = categoryService;
+    }
+
+    [HttpGet("getall")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = _categoryService.GetAllAsync();
+
+        return Ok(result);
+    }
+}
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebNetSample.Business.Abstract;
+
+namespace WebNetSample.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet("getall")]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _productService.GetProductDetailsAsync();
+
+        return Ok(result);
+    }
+}
