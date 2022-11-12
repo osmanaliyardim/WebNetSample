8d756265a4472815df4022b3bd48956dd297e59f
Osman Ali Yardım <osmanaliyardim@gmail.com>
2022-11-12 09:06:22 +0300

    FEATURE: API Consumers
    New two api consumers added as a Console App and a Html+JS

---
+++ WebNetConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using HttpClient client = new();
+
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Add("User-Agent", "EPAM WEB .NET Mentoring Program");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var categoriesJson = await client.GetStringAsync(
+    "https://localhost:7065/api/Categories/");
+
+    var productsJson = await client.GetStringAsync(
+    "https://localhost:7065/api/Products/");
+
+    Console.Write(categoriesJson);
+    Console.Write(productsJson);
+    Console.ReadLine();
+}
